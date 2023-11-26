bcfde7cbf33d7d0bba500e3a8a5c9e0d8126f939
Shivam-0804 <shivamchaudhary0408@gmail.com>
2023-11-26 22:28:20 +0530

    final_connect

--- php/data.php
+++ php/data.php
@@ -1,10 +1,11 @@
 <?php 
 $data = json_decode(file_get_contents('php://input'), true);
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-if (isset($data['cmp']) ) {
-    echo "Name received: " . $data['cmp']. $data['password'];
-} else {
-    echo "No name provided.";
-}
+    $wpm=$data['wpm'];
+    $time=$data['time'];
+    $rawSpeed=$data['rawSpeed'];
+    $consistency=$data['consistency'];
+    $accuracy=$data['accuracy'];
+    echo $wpm . " " . $time ;
 }
 ?>
